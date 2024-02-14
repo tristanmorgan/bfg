0acd1621aea73bfe01c324209fcd35b5053cd7b1
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2024-02-15 09:54:49 +1100

    fixup test table for compile.

--- parser/compile_test.go
+++ parser/compile_test.go
@@ -7,84 +7,76 @@ import (
 	"testing"
 )
 
-func TestCompileSmall(t *testing.T) {
-	program, err := Compile(bufio.NewReader(strings.NewReader(">>>>>[-]+++++[->>+<<]>>")))
-	want := []Instruction{
-		Instruction{opNoop, 0},
-		Instruction{opAddDp, 5},
-		Instruction{opZero, 0},
-		Instruction{opAddVal, 5},
-		Instruction{opMove, 2},
-		Instruction{opAddDp, 2},
+func TestCompile(t *testing.T) {
+	table := []struct {
+		name    string
+		code    string
+		program []Instruction
+	}{
+		{
+			"small_prog",
+			">>>>>[-]zero+++++[->>+<<]move>>",
+			[]Instruction{
+				Instruction{opNoop, 0},
+				Instruction{opAddDp, 5},
+				Instruction{opZero, 0},
+				Instruction{opAddVal, 5},
+				Instruction{opMove, 2},
+				Instruction{opAddDp, 2},
+			},
+		},
+		{
+			"op_dp",
+			">>>>>>><<<<<<>",
+			[]Instruction{
+				Instruction{opNoop, 0},
+				Instruction{opAddDp, 2},
+			},
+		},
+		{
+			"op_val",
+			"----++----++",
+			[]Instruction{
+				Instruction{opNoop, 0},
+				Instruction{opAddVal, -4},
+			},
+		},
+		{
+			"op_move",
+			"[->>+<<][<<<+>>>-]",
+			[]Instruction{
+				Instruction{opNoop, 0},
+				Instruction{opMove, 2},
+				Instruction{opMove, -3},
+			},
+		},
+		{
+			"op_jmp_z_nz",
+			"[->>+>+<<<]",
+			[]Instruction{
+				Instruction{opNoop, 0},
+				Instruction{opJmpZ, 8},
+				Instruction{opAddVal, -1},
+				Instruction{opAddDp, 2},
+				Instruction{opAddVal, 1},
+				Instruction{opAddDp, 1},
+				Instruction{opAddVal, 1},
+				Instruction{opAddDp, -3},
+				Instruction{opJmpNz, 1},
+			},
+		},
 	}
 
-	if !reflect.DeepEqual(program, want) {
-		t.Errorf("got %v want %v", program, want)
-	} else if err != nil {
-		t.Errorf("Error thrown  %v", err)
-	}
-}
+	for _, v := range table {
+		t.Run(v.name, func(t *testing.T) {
+			got, err := Compile(bufio.NewReader(strings.NewReader(v.code)))
+			want := v.program
 
-func TestCompileOpDp(t *testing.T) {
-	program, err := Compile(bufio.NewReader(strings.NewReader(">>>>>>><<<<<<>")))
-	want := []Instruction{
-		Instruction{opNoop, 0},
-		Instruction{opAddDp, 2},
-	}
-
-	if !reflect.DeepEqual(program, want) {
-		t.Errorf("got %v want %v", program, want)
-	} else if err != nil {
-		t.Errorf("Error thrown  %v", err)
-	}
-}
-
-func TestCompileOpVal(t *testing.T) {
-	program, err := Compile(bufio.NewReader(strings.NewReader("----++----++")))
-	want := []Instruction{
-		Instruction{opNoop, 0},
-		Instruction{opAddVal, -4},
-	}
-
-	if !reflect.DeepEqual(program, want) {
-		t.Errorf("got %v want %v", program, want)
-	} else if err != nil {
-		t.Errorf("Error thrown  %v", err)
-	}
-}
-
-func TestCompileOpMove(t *testing.T) {
-	program, err := Compile(bufio.NewReader(strings.NewReader("[->>+<<][<<<+>>>-]")))
-	want := []Instruction{
-		Instruction{opNoop, 0},
-		Instruction{opMove, 2},
-		Instruction{opMove, -3},
-	}
-
-	if !reflect.DeepEqual(program, want) {
-		t.Errorf("got %v want %v", program, want)
-	} else if err != nil {
-		t.Errorf("Error thrown  %v", err)
-	}
-}
-
-func TestCompileOpJmpZNZ(t *testing.T) {
-	program, err := Compile(bufio.NewReader(strings.NewReader("[->>+>+<<<]")))
-	want := []Instruction{
-		Instruction{opNoop, 0},
-		Instruction{opJmpZ, 8},
-		Instruction{opAddVal, -1},
-		Instruction{opAddDp, 2},
-		Instruction{opAddVal, 1},
-		Instruction{opAddDp, 1},
-		Instruction{opAddVal, 1},
-		Instruction{opAddDp, -3},
-		Instruction{opJmpNz, 1},
-	}
-
-	if !reflect.DeepEqual(program, want) {
-		t.Errorf("got %v want %v", program, want)
-	} else if err != nil {
-		t.Errorf("Error thrown  %v", err)
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v want %v", got, want)
+			} else if err != nil {
+				t.Errorf("Error thrown  %v", err)
+			}
+		})
 	}
 }
